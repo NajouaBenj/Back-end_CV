e11233ef90c40697537525f4bf191b594e5b1472
nissrine akkiyat <nissrineakkiyat@gmail.com>
2022-10-26 22:18:49 +0100

    competence

--- api/src/main/java/io/takima/demo/models/Competence.java
+++ api/src/main/java/io/takima/demo/models/Competence.java
@@ -1,4 +1,24 @@
 package io.takima.demo.models;
 
-public class Competence {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+public class Competence implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idc;
+    @Column
+    private String competence;
+    @Column
+    private String proficiency;
 }
